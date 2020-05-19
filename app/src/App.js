af32f504ee3a1a97cc5e4a057e9ae9f1f64df1fb
ChinDowns <webdev@chindowns.com>
2020-05-18 23:23:32 -0700

    render all images

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import images from "./images";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    images: images,
+    message: "Click to Begin",
+    score: 0,
+    highScore: 0
+  };
+
+  handleClick = (id, clicked) => {
+    const shuffleImgs = this.state.images;
+    if (clicked) {
+      shuffleImgs.forEach((image, index) => {
+        shuffleImgs[index].clicked = false;
+      });
+      return this.setState({
+        image: shuffleImgs.sort(() => Math.random() - 0.5),
+        message: "You Clicked that one already",
+        score:0
+      })
+    } else {
+      shuffleImgs.forEach((image, index) => {
+        if(id === image.id) {
+          shuffleImgs[index].clicked = true;
+        }
+      });
+      const {highScore, score} = this.state;
+      let newScore = score + 1;
+      let newHighScore = score > highScore ? newScore : highScore;
+
+      return this.setState({
+        image: shuffleImgs.sort(() => Math.random() -0.5),
+        message: "Good Guess!",
+        score: newScore,
+        highScore: newHighScore
+      })
+    }
+  };
+
+  render() {
+    return(
+      <div>
+      <Title
+        score={this.state.score}
+        highScore={this.state.highScore}
+        message={this.state.message}
+      />
+      {this.state.images.map(image => (
+      <ImageCard
+        key={image.id}
+        id={image.id}
+        name={images.name}
+        clicked={images.clicked}
+        image={images.image}
+        handleClick={this.handleClick}
+      />
+      ))};
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ app/src/images.js
@@ -0,0 +1,78 @@
+
+
+const images = [
+    {
+        "id": 1,
+        "name": "aquarius",
+        "clicked": false,
+        "image": '/images/aquarius.jpg'
+    },
+    {
+        "id": 2,
+        "name": "aries",
+        "clicked": false,
+        "image": '/images/aries.jpg'
+    },
+    {
+        "id": 3,
+        "name": "cancer",
+        "clicked": false,
+        "image": './images/cancer.jpg'
+    },
+    {
+        "id": 4,
+        "name": "capicorn",
+        "clicked": false,
+        "image": './images/capicorn.jpg'
+    },
+    {
+        "id": 5,
+        "name": "gemini",
+        "clicked": false,
+        "image": './images/gemini.jpg'
+    },
+    {
+        "id": 6,
+        "name": "leo",
+        "clicked": false,
+        "image": './images/leo.jpg'
+    },
+    {
+        "id": 7,
+        "name": "libra",
+        "clicked": false,
+        "image": './images/libra.jpg'
+    },
+    {
+        "id": 8,
+        "name": "pisces",
+        "clicked": false,
+        "image": './images/pisces.jpg'
+    },
+    {
+        "id": 9,
+        "name": "sagittarius",
+        "clicked": false,
+        "image": './images/sagittarius.jpg'
+    },
+    {
+        "id": 10,
+        "name": "scorpio",
+        "clicked": false,
+        "image": './images/scorpio.jpg'
+    },
+    {
+        "id": 11,
+        "name": "taurus",
+        "clicked": false,
+        "image": './images/taurus.jpg'
+    },
+    {
+        "id": 12,
+        "name": "virgo",
+        "clicked": false,
+        "image": './images/virgo.jpg'
+    }
+]
+
+export default images;
