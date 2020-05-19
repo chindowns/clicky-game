5f24852b3d1e5da653d0e2f61e1e5f2a6123acfe
ChinDowns <webdev@chindowns.com>
2020-05-19 09:51:06 -0700

    Updates

---
+++ precache-manifest.2e561170d2df0184b16febc49b0d648c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63fbbe888ad88a01823f744ba3cb3a2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "80b29147aff26d874e6e",
+    "url": "/clicky-game/static/css/main.eb08e9e9.chunk.css"
+  },
+  {
+    "revision": "59fd64409a57c4f8ca5a",
+    "url": "/clicky-game/static/js/2.1e496c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.1e496c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b29147aff26d874e6e",
+    "url": "/clicky-game/static/js/main.1c9bec28.chunk.js"
+  },
+  {
+    "revision": "88eb188ab9c94e8be32e",
+    "url": "/clicky-game/static/js/runtime-main.bc239b6b.js"
+  }
+]);
